7e2a6a45e06a06fc84c7ff16313e769cda566b81
uJotaQ <joao.gabrielss15253@gmail.com>
2026-03-04 18:40:54 -0300

    Initial Commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to the To-Do List App!</h1>
+      <p>Get started by creating your first to-do item.</p>
+    </div>
+  );
+};
+
+export default Home;
